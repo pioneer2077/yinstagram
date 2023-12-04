83fabde7c07851f2bc0d96accedd0d9a508fe178
pioneer2077 <134494401+pioneer2077@users.noreply.github.com>
2023-12-04 22:22:54 +0900

    feat: sanity setup & image resource

---
+++ src/app/image/nav/index.tsx
@@ -0,0 +1,8 @@
+export { default as AddPost } from "./AddPost.svg";
+export { default as Clicked_Home_icon } from "./Clicked_Home_icon.svg";
+export { default as Clicked_Search_Icon } from "./Clicked_Search_Icon.svg";
+export { default as Home_icon } from "./Home_icon.svg";
+export { default as Instagram_logo } from "./Instagram_logo.svg";
+export { default as Instagram } from "./Instagram.svg";
+export { default as Search } from "./Search.svg";
+export { default as Nav_hamburger } from "./nav_hamburger.svg";
